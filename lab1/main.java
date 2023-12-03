44e375bda2ca37fc2fd0e6d00b0037ed9acc58c7
tetr4dka <93588631+tetr4dka@users.noreply.github.com>
2023-12-04 01:13:18 +0200

    Update main.java

--- lab1/main.java
+++ lab1/main.java
@@ -2,17 +2,16 @@ package lab1;
 
 import java.util.function.Function;
 
-abstract class NumIntegration {
-    protected int numPoints;
-    protected double step;
-    protected double accuracy;
-    public NumIntegration(int numPoints, double step, double accuracy) {
-        this.numPoints = numPoints;
-        this.step = step;
-        this.accuracy = accuracy;
-    }
-    public abstract double calc(Function<Double, Double> expression, double lowerBound, double upperBound);
-    protected boolean isValidParameters() {
-        return numPoints > 0 && step > 0 && accuracy > 0;
+public class Main {
+    public static void main(String[] args) {
+        TrapezoidalIntegration trapezoidalIntegration = new TrapezoidalIntegration(1000, 0.001, 0.00001);
+        SimpsonIntegration simpsonIntegration = new SimpsonIntegration(1000, 0.001, 0.00001);
+        Function<Double, Double> expression = x -> x * x;
+        double lowerBound = 0;
+        double upperBound = 1;
+        double trapezoidalResult = trapezoidalIntegration.calc(expression, lowerBound, upperBound);
+        double simpsonResult = simpsonIntegration.calc(expression, lowerBound, upperBound);
+        System.out.println("Trapezoidal integration result: " + trapezoidalResult);
+        System.out.println("Simpson integration result: " + simpsonResult);
     }
 }
