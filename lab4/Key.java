15a207621cf875c2f5cb375c4f7053dda7e630ef
tetr4dka <93588631+tetr4dka@users.noreply.github.com>
2023-12-19 16:53:57 +0200

    added

---
+++ lab4/Key.java
@@ -0,0 +1,14 @@
+public class Key {
+    private String keyName;
+    public Key(String keyName) {
+        this.keyName = keyName;
+    }
+
+    public String getKeyName() {
+        return keyName;
+    }
+
+    public void setKeyName(String keyName) {
+        this.keyName = keyName;
+    }
+}
