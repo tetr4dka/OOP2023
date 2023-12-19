21ca09b6e04fd20f67157864cbe672b1280a26d6
tetr4dka <93588631+tetr4dka@users.noreply.github.com>
2023-12-19 16:53:31 +0200

    added

---
+++ lab4/Combination.java
@@ -0,0 +1,15 @@
+public class Combination {
+    private String combinationName;
+
+    public Combination(String combinationName) {
+        this.combinationName = combinationName;
+    }
+
+    public String getCombinationName() {
+        return combinationName;
+    }
+
+    public void setCombinationName(String combinationName) {
+        this.combinationName = combinationName;
+    }
+}
