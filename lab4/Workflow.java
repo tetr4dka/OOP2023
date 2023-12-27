1e1415a398df5b0d014dc31f4e33d0eafe5a87f8
tetr4dka <93588631+tetr4dka@users.noreply.github.com>
2023-12-27 23:21:33 +0200

    Delete lab4/Workflow.java

--- lab4/Workflow.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-import java.util.Map;
-
-public class Workflow {
-    public void showLastPerformedAction(List<Map.Entry<Key, String>> keys) {
-        Map.Entry<Key, String> action = keys.get(keys.size() - 1);
-        System.out.println(action.getKey().getKeyName() + " | " + action.getValue());
-    }
-}
