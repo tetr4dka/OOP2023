7fab48d868ac7960ab3f00b8b13857fc2fee6867
tetr4dka <93588631+tetr4dka@users.noreply.github.com>
2023-12-27 23:21:26 +0200

    Delete lab4/VKeyboardInterface.java

--- lab4/VKeyboardInterface.java
+++
@@ -1,6 +0,0 @@
-public interface VKeyboardInterface {
-    public void pressKey(Key key);
-    public void pressCombination(Combination combination);
-    public void reset();
-    public void changeCombination(Combination target, Combination newCombination);
-}
